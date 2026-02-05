3c3696d551853dc6acae7b23e639a8523fec35d6
IgorSantanaM <igorsantanamedeiros17@gmail.com>
2026-02-04 21:40:29 -0400

    (feat): Added creating Employee commands

---
+++ backend/src/DottIn.Application/Features/Employees/Commands/CreateEmployee/CreateEmployeeCommand.cs
@@ -0,0 +1,17 @@
+﻿using DottIn.Application.Shared.DTOS;
+using MediatR;
+
+namespace DottIn.Application.Features.Employees.Commands.CreateEmployee;
+
+public record CreateEmployeeCommand(
+    string Name,
+    DocumentDTO Document,
+    Stream ImageStream,
+    string ImageFileName,
+    string ImageContentType,
+    Guid BranchId,
+    TimeOnly StartWorkTime,
+    TimeOnly EndWorkTime,
+    TimeOnly IntervalStart,
+    TimeOnly IntervalEnd)
+    : IRequest<Guid>;
---
+++ backend/src/DottIn.Application/Shared/DTOS/DocumentDTO.cs
@@ -0,0 +1,6 @@
+﻿using DottIn.Domain.ValueObjects;
+
+namespace DottIn.Application.Shared.DTOS;
+
+public record DocumentDTO(string Value, DocumentType Type);
+
